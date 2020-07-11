0182ba563e26a5f702557a6a1077849350c0f72c
Kirill Sytnyanskiy <nyan.kirill@gmail.com>
2020-07-11 16:44:38 +0600

    Updates

---
+++ precache-manifest.323db1a554a00b7d981fc6f2d7358d8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2507a2ad34e30269ef62d76db9f7c27",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "f34ff7c7cfe4e79d1af8",
+    "url": "/kanban/static/css/main.9eee8a91.chunk.css"
+  },
+  {
+    "revision": "11e27b5c73d9bdd756ba",
+    "url": "/kanban/static/js/2.42421a1f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kanban/static/js/2.42421a1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f34ff7c7cfe4e79d1af8",
+    "url": "/kanban/static/js/main.b602b17b.chunk.js"
+  },
+  {
+    "revision": "f5aa037a105c127e43db",
+    "url": "/kanban/static/js/runtime-main.72bc9640.js"
+  }
+]);
